1c4dff01f28d7eb4674fe5609ecb30ab1e4b4dd8
iliangogov <iliangogov@gmail.com>
2016-12-29 00:36:46 +0200

    .

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
     constructor(private http: Http) { }
 
     getAll() {
-        return this.http.get('http://localhost:3000/api/users').map((response: Response) => response.json());
+        return this.http.get('https://holiday-advisor.herokuapp.com/api/users').map((response: Response) => response.json());
     }
 
     // getById(id: number) {
@@ -17,7 +17,7 @@ export class UserService {
     // }
 
     create(user: User) {
-        return this.http.post('http://localhost:3000/api/signup', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('https://holiday-advisor.herokuapp.com/api/signup', user, this.jwt()).map((response: Response) => response.json());
     }
 
     // update(user: User) {
