957ae8f250519e29473f93061744b1518f063460
iliangogov <iliangogov@gmail.com>
2016-12-30 13:09:07 +0200

    .

---
+++ src/app/_directives/dropdown/dropdown.module.ts
@@ -0,0 +1,24 @@
+// import {NgModule} from "@angular/core";
+// import {CommonModule} from "@angular/common";
+// import {DropdownNotClosableZone} from "./dropdown-not-closable-zone.directive";
+// import {Dropdown} from "./dropdown.directive";
+// import {DropdownOpen} from "./dropdown-open.directive";
+
+// @NgModule({
+//     imports: [
+//         CommonModule
+//     ],
+//     declarations: [
+//         DropdownNotClosableZone,
+//         Dropdown,
+//         DropdownOpen,
+//     ],
+//     exports: [
+//         DropdownNotClosableZone,
+//         Dropdown,
+//         DropdownOpen,
+//     ]
+// })
+// export class DropdownModule {
+
+// }
---
+++ src/app/_directives/dropdown/index.ts
@@ -0,0 +1,4 @@
+export * from './dropdown-not-closable-zone.directive';
+export * from './dropdown.directive';
+export * from './dropdown-open.directive';
+// export * from './dropdown.module'
