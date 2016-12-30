9f88d975d6d5a36569b75e118e273887f3a8c309
iliangogov <iliangogov@gmail.com>
2016-12-31 00:48:58 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import {AuthenticationService} from './_services/index';
 import { Router } from '@angular/router';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations:[
+  animations: [
     trigger('movePanel',[
       transition('void => *', [
         style({transform: 'translateY(-100%)'}),
@@ -17,8 +16,10 @@ import { Router } from '@angular/router';
     ])
   ]
 })
+
 export class AppComponent {
-  constructor(private authService:AuthenticationService,private router:Router){}
+  isVisible: boolean = true;
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   get isLoggedIn(){
     return this.authService.isLoggedIn();
--- src/app/page-components/google-map/google-map.component.ts
+++ src/app/page-components/google-map/google-map.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, trigger, state, style, animate, transition } from '@angular/core';
 
 @Component({
     selector: 'app-googlemap',
     templateUrl: './google-map.component.html',
-    styleUrls: ['./google-map.component.css']
+    styleUrls: ['./google-map.component.css'],
+    animations: [
+        trigger('showmsg', [
+      state('show', style({opacity: '1', height: '*'})),
+      state('hide', style({opacity: '0', height: '0px'})),
+      transition('show <=> hide', [
+        animate(1000)
+      ])
+    ])
+    ]
 })
 
 export class GoogleMapComponent {
     @Input() lat: number;
     @Input() lng: number;
+    state: string= 'hide';
+
+    togglestates() {
+    this.state = (this.state === 'hide' ? 'show' : 'hide');
+    }
 }
